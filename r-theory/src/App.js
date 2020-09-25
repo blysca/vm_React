f38d9939098b5da0b5df21e25bc98ee835177d82
blysca <blysca@gmail.com>
2020-09-25 19:39:38 +0300

    init quiz app

--- r-theory/src/App.js
+++ r-theory/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Car from './components/Car'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   state = {
--- r-theory/src/components/Car.js
+++ r-theory/src/components/Car.js
@@ -1,33 +1,63 @@
-import React from 'react'
-import './Car.css'
+import React, { Component } from 'react'
+import Radium from 'radium'
+import classes from './Car.module.scss'
 
-export default (props) => {
-  return (
-  <div className="max-w-sm rounded overflow-hidden shadow-lg mb-20 my-8">
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">{props.name}</div>
-      <p className="text-gray-700 text-base">{props.years}</p>
-      <div className="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={props.name}>
-        New Name
-      </label>
-      <input 
-        onChange={props.onChangeName}
-        className="shadow appearance-none border border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
-        id={props.name}
-        type="text"
-        placeholder="Enter new name" 
-        value={props.name}
-      />
-    </div>
-    <div className="flex items-center justify-between">
-      <button
-        onClick={props.onDelete}
-        className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="button"
-        >Delete</button>     
-    </div>
-    </div>
-  </div>
-  )
+class Car extends Component {
+  render() {
+    const inputClasses = [
+      'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none',
+    ]
+
+    if (this.props.name) {
+      inputClasses.push('border-blue-500')
+    } else {
+      inputClasses.push('border-red-500')
+    }
+
+    const style = {
+      ':hover': {
+        boxShadow:
+          '0 4px 6px -1px rgba(0,0,0,.1), 0 2px 4px -1px rgba(0,0,0,.06)',
+      },
+    }
+
+    return (
+      <div
+        style={style}
+        className='max-w-sm rounded overflow-hidden shadow-sm mb-20 my-8'
+      >
+        <div className={classes.car + ' px-6 py-4'}>
+          <div className='font-bold text-xl mb-2'>Car name: {this.props.name}</div>
+          <p className='text-gray-700 text-base'>{this.props.years}</p>
+          <div className='mb-6'>
+            <label
+              className='block text-gray-700 text-sm font-bold mb-2'
+              htmlFor={this.props.name}
+            >
+              New Name
+            </label>
+            <input
+              onChange={this.props.onChangeName}
+              className={inputClasses.join(' ')}
+              id={this.props.name}
+              type='text'
+              placeholder='Enter new name'
+              value={this.props.name}
+            />
+          </div>
+          <div className='flex items-center justify-between'>
+            <button
+              onClick={this.props.onDelete}
+              className='bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+              type='button'
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Radium(Car)
